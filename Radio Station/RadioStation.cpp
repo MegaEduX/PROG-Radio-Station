47d6ba6ce3037f021bf412fc6f1a937b3e9d6a4a
Eduardo Almeida <megaedux@me.com>
2013-05-15 01:10:15 +0100

    Merged changes from katukii.

---
+++ Radio Station/RadioStation.cpp
@@ -0,0 +1,20 @@
+#include "RadioStation.h"
+#include "Music.h"
+#include "Playlist.h"
+#include <vector>
+
+
+bool RadioStation::setName(std::string name){
+	if (name.size <= 0)
+		return false;
+	
+	_name = name;
+	
+	return true;
+}
+
+void RadioStation::generateSet(){
+	_currentPlaySet = _allTracks;
+
+	_currentPlaySet.shufflePlaylist();
+}
