d555009f894fecf250f0fb926f8b12bc4cee5f23
Eduardo Almeida <megaedux@me.com>
2013-05-19 20:39:25 +0100

    User Manager is quite possibly complete.

--- Radio Station/UserManager.cpp
+++ Radio Station/UserManager.cpp
@@ -22,25 +22,53 @@ UserManager::UserManager() {
 }
 
 bool UserManager::addUser(User *aUser) {
+    for (int i = 0; i < _userVector.size(); i++)
+        if (_userVector[i] -> userId() == aUser -> userId())
+            return false;
     
+    _userVector.push_back(aUser);
+    
+    return true;
 }
 
 bool UserManager::removeUser(User *aUser) {
+    for (int i = 0; i < _userVector.size(); i++)
+        if (_userVector[i] -> userId() == aUser -> userId()) {
+            _userVector.erase(_userVector.begin() + i);
+            
+            return true;
+        }
     
+    return false;
 }
 
 bool UserManager::removeUser(int userId) {
+    for (int i = 0; i < _userVector.size(); i++)
+        if (_userVector[i] -> userId() == userId) {
+            _userVector.erase(_userVector.begin() + i);
+            
+            return true;
+        }
     
+    return false;
 }
 
 User* UserManager::getUser(int userId) {
+    for (int i = 0; i < _userVector.size(); i++)
+        if (_userVector[i] -> userId() == userId)
+            return _userVector[i];
     
+    return NULL;
 }
 
 User* UserManager::getUser(std::string userName) {
+    for (int i = 0; i < _userVector.size(); i++)
+        if (!(_userVector[i] -> name().compare(userName)))
+            return _userVector[i];
     
+    return NULL;
 }
 
 const unsigned int UserManager::userCount() {
-    
+    return (const unsigned int) _userVector.size();
 }
