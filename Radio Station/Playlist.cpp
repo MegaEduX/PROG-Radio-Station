6f0b7f627a3ebd80f3c7fc9841fe0cc455ce9adc
Joao <netforum@gmail.com>
2013-05-19 17:30:28 +0100

    Made some changes at Playlist.h and created Playlist.cpp to start working on it.
    #TRABALHAFDP

---
+++ Radio Station/Playlist.cpp
@@ -0,0 +1,20 @@
+//
+//  User.cpp
+//  Radio Station
+//
+//  Created by Jo‹o Almeida on 19/05/13.
+//
+#include "Playlist.h"
+#include "Music.h"
+
+void Playlist::addSong(Music *theSong, int playcount) {
+}
+
+void Playlist::removeSong(Music *theSong){
+}
+
+const std::vector<Music *> Playlist::search(int musicId, std::string title,  std::string artist, std::string author , std::string album,std::string music_genre, int year){
+}
+
+void Playlist::shuffle(){
+}
--- Radio Station/Playlist.h
+++ Radio Station/Playlist.h
@@ -40,7 +40,7 @@ public:
     
     // Searches through the playlist and returns a vector with matches.
 
-	const std::vector<Music *> search(int musicId, std::string title, int year, std::string artist, std::string music_genre);
+	const std::vector<Music *> search(int musicId, std::string title,  std::string artist, std::string author , std::string album,std::string music_genre, int year);
     
     unsigned int count(); // Adicionado por razoes obvias
     
