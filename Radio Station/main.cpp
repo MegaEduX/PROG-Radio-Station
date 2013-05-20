c20c9b2befc554323ae3abd718d1cc0eda59b33d
Eduardo Almeida <megaedux@me.com>
2013-05-20 16:21:44 +0100

    Fixed Katukii's code a bit. It's still a complete mess tho! #TRABALHAKATUKII

--- Radio Station/main.cpp
+++ Radio Station/main.cpp
@@ -8,110 +8,111 @@
 
 #include <iostream>
 #include <string>
+#include <ctype.h>
+
 #include "UserManager.h"
 #include "User.h"
-#include <ctype>
+
+#include "RadioStation.h"
+
 #include "Playlist.h"
 
-
-void Login(){
-	
+void login(){
 	User *theUser = NULL;
 
-		do {
-			std::cout << "Enter ID or Username: ";
-			std::getline(std::cin, username);
-
-			if (atoi(username.c_str()))
-				theUser = UserManager::Instance()->getUser(atoi(username.c_str()));
-			else
-				theUser = UserManager::Instance()->getUser(username);
-
-			if (!theUser)
-				std::cout << "ID/Username not found! Try again!" << std::endl;
-			
-
-		} while (!theUser);		
+    do {
+        std::string username;
+        
+        std::cout << "Enter ID or Username: ";
+        std::getline(std::cin, username);
+        
+        if (atoi(username.c_str()))
+            theUser = UserManager::Instance()->getUser(atoi(username.c_str()));
+        else
+            theUser = UserManager::Instance()->getUser(username);
+        
+        if (!theUser)
+            std::cout << "ID/Username not found! Try again!" << std::endl;
+        
+        
+    } while (!theUser);
 }
 		
-void NewUser(){
-
-	std::string age;
-	char sex;
+void newUser() {
+	int age = 0;
+    
 	std::string name;
+    std::string username;
+    
+    char sex;
 
-
-		std::cout << "Username: ";
-		std::getline(std::cin, username);
-		std::cout << std::endl;
-
-		do{
-
-		std::cout << "Age: ";
+    std::cout << "Username: ";
+    std::getline(std::cin, username);
+    std::cout << std::endl;
+    
+    do {
+        std::cout << "Age: ";
 		std::cin >> age;
-		atoi(age.c_str());
 		std::cout << std::endl;
-
-		} while (!atoi(age.c_str()));
-
-		do{
-
+    } while (!age);
+    
+    do {
 		std::cout << "Gender(M/F): ";
 		std::cin >> sex;
 		std::cout << std::endl;
 		sex=toupper(sex);
 		
-		}while (!( sex == 'M' || sex == 'F' ));
-
-		do{
-
+    } while (!(sex == 'M' || sex == 'F'));
+    
+    do {
 		std::cout << "Full Name: ";
 		std::getline(std::cin, name);
 		std::cout << std::endl;
-
-		} while (atoi(name.c_str()));
-
-		Playlist newPlaylist;
-
-		theUser = new User(UserManager::Instance()->userCount(), age, (sex == 'F' ? kSexFemale : kSexMale), name, newPlaylist);
-
+    } while (atoi(name.c_str()));
+    
+    Playlist newPlaylist;
+    
+    User *theUser = new User(UserManager::Instance()->userCount(), age, (sex == 'F' ? kSexFemale : kSexMale), name, newPlaylist);
+    
+    if (UserManager::Instance() -> addUser(theUser))
+        std::cout << "User Add: Success!" << std::endl;
 }
 
-void startscreen(){
-	
+void start() {
 	int opc = 0;
 	
 	std::string username;
 
-	std::cout << "HELLO AND WELCOME TO THE RADIO!\n" << std::endl;
+	std::cout << "Welcome to " << (RadioStation::Instance() -> name().compare("") != 0 ? RadioStation::Instance() -> name() : "our radio station!") << std::endl << std::endl;
 	std::cout << "1. Login (ID or Username) \n";
 	std::cout << "2. Create New User\n";
-
+    std::cout << std::endl;
+    std::cout << "Select an Option: ";
 	std::cin >> opc;
 
-	switch (opc){
+	switch (opc) {
 
-	case 1 :
-
-		Login();
+        case 1:
+            
+            login();
 			
-		break;
-
-	case 2:
-
-		NewUser();
-
-		break;
+            break;
+            
+        case 2:
+            
+            newUser();
+            
+            break;
+            
+        default:
+            
+            break;
 	}
 
 }
 
-int main(int argc, const char * argv[]){
+int main() {
+	start();
 
-	startscreen();
-
-	system("pause");
 	return 0;
-
-
 }
