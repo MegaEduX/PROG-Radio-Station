a3a37082655b2dd9e525bc8960f44fa1ac4dbb6c
Eduardo Almeida <megaedux@me.com>
2013-05-20 18:10:30 +0100

    A bit of work on fixing the user interface...

--- Radio Station/main.cpp
+++ Radio Station/main.cpp
@@ -17,6 +17,8 @@
 
 #include "Playlist.h"
 
+void start();
+
 void login(){
 	User *theUser = NULL;
 
@@ -24,7 +26,12 @@ void login(){
         std::string username;
         
         std::cout << "Enter ID or Username: ";
-        std::getline(std::cin, username);
+        std::cin >> username;
+        
+        if (!username.compare("-1")) {
+            std::cout << "Operation Canceled." << std::endl << std::endl;
+            start();
+        }
         
         if (atoi(username.c_str()))
             theUser = UserManager::Instance()->getUser(atoi(username.c_str()));
@@ -32,7 +39,7 @@ void login(){
             theUser = UserManager::Instance()->getUser(username);
         
         if (!theUser)
-            std::cout << "ID/Username not found! Try again!" << std::endl;
+            std::cout << "ID/Username not found! Try again! You may also cancel with '-1'." << std::endl;
         
         
     } while (!theUser);
@@ -47,26 +54,32 @@ void newUser() {
     char sex;
 
     std::cout << "Username: ";
-    std::getline(std::cin, username);
+    std::cin >> username;
+    
     std::cout << std::endl;
     
     do {
         std::cout << "Age: ";
 		std::cin >> age;
+        
 		std::cout << std::endl;
     } while (!age);
     
     do {
 		std::cout << "Gender(M/F): ";
 		std::cin >> sex;
+        
 		std::cout << std::endl;
-		sex=toupper(sex);
+		
+        sex = toupper(sex);
 		
     } while (!(sex == 'M' || sex == 'F'));
     
     do {
 		std::cout << "Full Name: ";
-		std::getline(std::cin, name);
+		
+        std::cin >> name;
+        
 		std::cout << std::endl;
     } while (atoi(name.c_str()));
     
@@ -75,7 +88,9 @@ void newUser() {
     User *theUser = new User(UserManager::Instance()->userCount(), age, (sex == 'F' ? kSexFemale : kSexMale), name, newPlaylist);
     
     if (UserManager::Instance() -> addUser(theUser))
-        std::cout << "User Add: Success!" << std::endl;
+        std::cout << "You were registeried. You may now login!" << std::endl << std::endl;
+    else
+        std::cout << "There was a problem. Please try again." << std::endl << std::endl;
 }
 
 void start() {
@@ -84,9 +99,16 @@ void start() {
 	std::string username;
 
 	std::cout << "Welcome to " << (RadioStation::Instance() -> name().compare("") != 0 ? RadioStation::Instance() -> name() : "our radio station!") << std::endl << std::endl;
-	std::cout << "1. Login (ID or Username) \n";
-	std::cout << "2. Create New User\n";
+    
+	std::cout << "1. Existing Users: Login!" << std::endl;
+	std::cout << "2. New Users: Register!" << std::endl;
+    
     std::cout << std::endl;
+    
+    std::cout << "Press any other key to quit." << std::endl;
+    
+    std::cout << std::endl;
+    
     std::cout << "Select an Option: ";
 	std::cin >> opc;
 
@@ -105,6 +127,8 @@ void start() {
             break;
             
         default:
+            
+            exit(0);
             
             break;
 	}
